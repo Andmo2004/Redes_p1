9ab1a4d004f944ce81e32b34954d5f6e3dc278d0
MiguelAngel <mmontijanosempere@gmail.com>
2024-10-12 14:16:35 +0200

    Update ficheros.cpp

--- ficheros.cpp
+++ ficheros.cpp
@@ -9,6 +9,12 @@ void rellenaVectorUsuario(vector<string> &usernames, vector<string> &passwords){
 
     ofstream fich;
 
-    
+    fich.open("Users_Passwords.txt");
 
+    for(int i=0;i<usernames.size();i++){
+
+        fich<<usernames[i]<<"\t"<<passwords[i]<<endl;
+    }
+    
+    fich.close();
 }
